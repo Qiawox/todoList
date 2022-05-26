a8784ab49152360e6ac3b3b253242917fef3feac
Qiawox <gv7361299@gmail.com>
2022-05-26 13:28:38 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
 const todo = {
+//   main() {
+    
+// //   this.save();
+// // },
   
+
+
+
   action(e) {
     const target = e.target;
     if (target.classList.contains('todo__action')) {
       const action = target.dataset.todoAction;
       const elemItem = target.closest('.todo__item');
+      
       if (action === 'deleted' && elemItem.dataset.todoState === 'deleted') {
         elemItem.remove();
       } else {
         elemItem.dataset.todoState = action; 
-        $("button.nnn").on('click', function(){
-  
-          for (let i = 0; i < 1; i++) {
-            // document.querySelector('.todo__items').setAttribute('data-todo-option', 'completed');
-            document.querySelector('.todo__item').setAttribute('data-todo-state', 'completed');
-            save();
-          }
-          
-        })
       }
       this.save();
     } else if (target.classList.contains('todo__add')) {
@@ -36,9 +35,9 @@ const todo = {
   create(text) {
     return `<li class="todo__item" data-todo-state="active">
       <span class="todo__task">${text}</span>
-      <span class="todo__action todo__action_restore" data-todo-action="active"></span>
-      <span class="todo__action todo__action_complete" data-todo-action="completed"></span>
-      <span class="todo__action todo__action_delete" data-todo-action="deleted"></span></li>`;
+      <button class="todo__action todo__action_restore" data-todo-action="active">Unready</button>
+      <button class="todo__action todo__action_complete" data-todo-action="completed">Ready</button>
+      <button class="todo__action todo__action_delete" data-todo-action="deleted">Delete</button></li>`;
   },
   init() {
     const fromStorage = localStorage.getItem('todo');
